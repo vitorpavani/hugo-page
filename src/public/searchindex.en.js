3fcec41c694d387716d7193524581bd48006f81d
Vitor Pavan <vipavani@hotmail.com>
2024-10-27 21:34:17 +0100

    add theme and init posts

---
+++ src/public/searchindex.en.js
@@ -0,0 +1,77 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "My New Hugo Site",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Site \u003e Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: Css",
+    "uri": "/tags/css/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Site \u003e Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: Html",
+    "uri": "/tags/html/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "My New Hugo Site",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Site",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Posts",
+    "uri": "/posts/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Site \u003e Posts",
+    "content": "This is the second post.",
+    "description": "This is the second post.",
+    "tags": [],
+    "title": "Second post",
+    "uri": "/posts/second/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Site",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Site \u003e Categories",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Category :: Tech",
+    "uri": "/categories/tech/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Site \u003e Posts",
+    "content": "This is a heading one This is a heading two This is a paragraph.\nfsdfsdfsddadad",
+    "description": "This is a heading one This is a heading two This is a paragraph.\nfsdfsdfsddadad",
+    "tags": [
+      "Html",
+      "Css"
+    ],
+    "title": "This is the first post on Hugo",
+    "uri": "/posts/first/index.html"
+  }
+]
